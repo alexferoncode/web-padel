44d5f0e3a59d22fa0c6479d6b6423030d6cb803f
alexferoncode <alexferoncode@gmail.com>
2025-11-29 10:33:02 +0100

    Login primera version

--- src/pages/Pagina_Login.tsx
+++ src/pages/Pagina_Login.tsx
@@ -1,20 +1,13 @@
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
-import { supabase } from "../../supabaseClient"; // Asegúrate de que tu cliente está exportado aquí
+import Navbar from "../componentes/Navbar/Navbar";
+import Login from "../componentes/Login/Login";
+import Footer from "../componentes/Footer/Footer";
 
 export default function Pagina_Login() {
   return (
-    <div style={{ maxWidth: "420px", margin: "96px auto" }}>
-      <h1>Supabase + React</h1>
-      <p>Sign in via magic link or OAuth providers</p>
-
-      <Auth
-        supabaseClient={supabase}
-        appearance={{ theme: ThemeSupa }}
-        theme="dark"
-        providers={[]} // opcional: ["google", "github"]
-        redirectTo={window.location.origin} // dónde redirige después del login
-      />
-    </div>
+    <>
+      <Navbar />
+      <Login />
+      <Footer />
+    </>
   );
 }
